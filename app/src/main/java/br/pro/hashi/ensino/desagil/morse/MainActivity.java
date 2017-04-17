892bf6a93d05871380f498940dbb5fb3db15891e
Jean Low <j.luca@hotmail.com>
2017-04-17 11:58:43 -0300

    finished... probably

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MainActivity.java
@@ -2,11 +2,14 @@ package br.pro.hashi.ensino.desagil.morse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends UtilityActivity {
     private TextView titleText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         titleText = (TextView) findViewById(R.id.titleText);
+        button = (Button) findViewById(R.id.button);
 
-        titleText.setText("Welcome to my world of pain!");
+    }
+
+    public void seeTheTruth(View v){
+        Utilities.confirm(this,"do you wanna se the truth?");
+    }
+
+    public void listenConfirm(boolean answer){
+
+        if(answer){
+            titleText.setText("Welcome to my world of pain!");
+        }
+        else{
+            titleText.setText("ok then");
+        }
+
     }
 }
