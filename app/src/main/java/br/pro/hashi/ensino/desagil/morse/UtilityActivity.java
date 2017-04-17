33e9bae43231ce69a9e1a9dba6518ffb143387b4
Jean Low <j.luca@hotmail.com>
2017-04-17 11:25:32 -0300

    criado função e classe de popup de confirmação

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/UtilityActivity.java
@@ -0,0 +1,16 @@
+package br.pro.hashi.ensino.desagil.morse;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jean-low on 17/04/17.
+ */
+
+abstract class UtilityActivity extends AppCompatActivity {
+
+
+    public void listenConfirm(boolean isConfirmed){
+
+    }
+}
