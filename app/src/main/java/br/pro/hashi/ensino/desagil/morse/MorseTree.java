e85abf1e0ce73a5aa4c095c5d0131b4a0f9393a6
SabrinaSimao <sabrina_simao@hotmail.com>
2017-04-23 19:49:11 -0300

    Revert "trabalhando na morse tree"
    This reverts commit c4142de927e946adc68cf0f418831aa515c553df.

--- app/src/main/java/br/pro/hashi/ensino/desagil/morse/MorseTree.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/morse/MorseTree.java
@@ -1,15 +1,7 @@
 package br.pro.hashi.ensino.desagil.morse;
 
-import org.w3c.dom.Node;
-//https://www.w3.org/2003/01/dom2-javadoc/org/w3c/dom/Node.html documentacao do Node
-
 public class MorseTree {
     public char translate(String code) {
         return '?';
     }
-
-    private Node[] nodes;
-    private String[] MorseTree = {};
-
-
 }
